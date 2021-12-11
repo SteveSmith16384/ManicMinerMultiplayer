28e3a5cb5dc732526cc113b75824cd569a88214c
= <stephen.carlylesmith@googlemail.com>
2021-12-11 16:13:25 +0000

    Added basic yellow robot

--- src/DroppingFloor.gd
+++ src/DroppingFloor.gd
@@ -1,16 +1,34 @@
 extends StaticBody2D
 
 var start_y : int
+var bodies = []
+var time_to_raise : float
 
 func _ready():
 	start_y = self.position.y
 	pass
 	
 	
+func _process(delta):
+	if bodies.size() > 0:
+		time_to_raise = 0
+#		if self.position.y < start_y + $Sprite.texture.get_height() * 2:
+		self.position.y += delta * 10
+	else:
+		time_to_raise += delta
+		if time_to_raise > 5:
+			if position.y > start_y:
+				self.position.y -= delta * 10
+	pass
+	
 	
 func _on_Area2D_Dropping_body_entered(body):
 	if body.is_in_group("players"):
-		self.position.y += 1
-		if self.position.y > start_y + $Sprite.texture.get_height() * 2:
-			self.queue_free()
+		bodies.push_back(body)
+	pass
+
+
+func _on_Area2D_Dropping_body_exited(body):
+	if body.is_in_group("players"):
+		bodies.remove(bodies.find(body))
 	pass
